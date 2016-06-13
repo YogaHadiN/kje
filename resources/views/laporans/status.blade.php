b231418fcb784135f24f1c257d9f198c47d9417e
Yogaman89 <yoga_email@yahoo.com>
2016-06-13 23:12:18 +0700

    munculkan transaksi di laporan status

--- resources/views/laporans/status.blade.php
+++ resources/views/laporans/status.blade.php
@@ -105,6 +105,39 @@ Klinik Jati Elok | Laporan Pemeriksaan
                         </div>
                         {!! $periksa->terapi_html !!}
                         <div>
+            <table class="table borderless table-condensed">
+                <thead>
+                    <tr>
+                        <th>Akun </th>
+                        <th>Debet</th>
+                        <th>Kredit</th>
+                    </tr>
+                </thead>
+                <tbody>
+                      @if (count($periksa->jurnals) > 0)
+                        @foreach($periksa->jurnals as $k=>$jur)
+                            @if($jur->debit == 1)
+                               <tr>
+                                   <td>{!! $jur->coa->coa !!}</td>
+                                   <td class="uang">{!! $jur->nilai!!}</td>
+                                   <td></td>
+                               </tr> 
+                               @else
+                                <tr>
+                                   <td class="text-right">{!! $jur->coa->coa !!}</td>
+                                   <td></td>
+                                   <td class="uang">{!! $jur->nilai!!}</td>
+                                </tr>
+                            @endif
+                            
+                        @endforeach
+                      @else
+                        <tr>
+                          <td colspan="7" class="text-center">Tidak ada Data Untuk Ditampilkan :p</td>
+                        </tr>
+                      @endif
+                </tbody>
+            </table>
                             {!! $periksa->jurnals !!}
                         </div>
                       </td>
