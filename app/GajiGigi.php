c585c8ea6da5b484d3826f6e9a034a2ceb50db9b
yoga <yoga_email@yahoo.com>
2016-08-15 14:19:32 +0700

    Perbaiki jurnal di GajiGigi

--- app/GajiGigi.php
+++ app/GajiGigi.php
@@ -8,11 +8,19 @@ class GajiGigi extends Model
 {
 	protected $dates = ['tanggal_mulai', 'tanggal_akhir'];
 	public function staf(){
-		return $this->belongsTo('App\Staf');
+		return $this->belongsTo('App\GajiGigi');
 	}
 
 	public function getPeriodeAttribute(){
 		return $this->tanggal_mulai->format('d-m-Y') . ' s/d ' . $this->tanggal_akhir->format('d-m-Y');
 	}
 	
+    public function getKetjurnalAttribute(){
+		$temp = 'Pembayaran Gaji dokter gigi a/n ';
+		$temp .= $this->staf->nama . '</strong><br />';
+		$temp .= 'dilakukan oleh ' $this->petugas->nama;
+		$temp .= '<br />dibayar pada tanggal' $this->tanggal_dibayar;
+		return $temp;
+    }
+
 }
