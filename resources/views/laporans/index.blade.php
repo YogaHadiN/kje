c73b0261cd302f186472346da131ef386b5db226
yoga <yoga_email@yahoo.com>
2016-09-22 23:18:07 +0700

    table-responsive di laporan index.blade.php

--- resources/views/laporans/index.blade.php
+++ resources/views/laporans/index.blade.php
@@ -137,41 +137,43 @@ Klinik Jati Elok | Home
                             RINGKASAN PASIEN HARI INI 
                     </div>
                     <div class="panel-body">
-                        <table class="table table-condensed table-hover">
-						  <thead>
-							  <tr>
-								  <th>Nama Asuransi</th>
-								  <th>Jumlah</th>
-								  @foreach($polis as $poli)	
-									  <th>{{ $poli }}</th>
-								  @endforeach
-							  </tr>
-						  </thead>
-						  <tbody>
-							  @if (count($hariinis) > 0)
-								  @foreach ($hariinis as $hariini)
-									  <tr>
-										  <td>{!! $hariini->nama !!}</td>
-										  <th>{!! $hariini->jumlah !!}</th>
-										  @foreach($polis as $poli)	
-											  <td class="text-center">{{ App\Classes\Yoga::periksa_by_asuransi($tanggal, $hariini->id, $poli) }}</td>
-										  @endforeach
-									  </tr>
-								  @endforeach
-							  @else
+						<div class="table-responsive">
+							<table class="table table-condensed table-hover">
+							  <thead>
 								  <tr>
-									  <td colspan="2" class="text-center">Tidak ada data untuk ditampilkan :p</td>
+									  <th>Nama Asuransi</th>
+									  <th>Jumlah</th>
+									  @foreach($polis as $poli)	
+										  <th>{{ $poli }}</th>
+									  @endforeach
 								  </tr>
-							  @endif
-						  </tbody>
-						  <tfoot>
-							  <th> Jumlah </th>
-							  <th>{!! count($periksas) !!}</th>
-							  @foreach($polis as $poli)	
-								  <th class="text-center">{{ App\Classes\Yoga::periksa_by_poli($tanggal, $poli) }}</th>
-							  @endforeach
-						  </tfoot>
-					  </table>
+							  </thead>
+							  <tbody>
+								  @if (count($hariinis) > 0)
+									  @foreach ($hariinis as $hariini)
+										  <tr>
+											  <td>{!! $hariini->nama !!}</td>
+											  <th>{!! $hariini->jumlah !!}</th>
+											  @foreach($polis as $poli)	
+												  <td class="text-center">{{ App\Classes\Yoga::periksa_by_asuransi($tanggal, $hariini->id, $poli) }}</td>
+											  @endforeach
+										  </tr>
+									  @endforeach
+								  @else
+									  <tr>
+										  <td colspan="2" class="text-center">Tidak ada data untuk ditampilkan :p</td>
+									  </tr>
+								  @endif
+							  </tbody>
+							  <tfoot>
+								  <th> Jumlah </th>
+								  <th>{!! count($periksas) !!}</th>
+								  @foreach($polis as $poli)	
+									  <th class="text-center">{{ App\Classes\Yoga::periksa_by_poli($tanggal, $poli) }}</th>
+								  @endforeach
+							  </tfoot>
+						  </table>
+						</div>
                     </div>
                 </div>
 			</div>
