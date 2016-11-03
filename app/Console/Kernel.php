006560b1c02755fdb01cbbb6554020f7b1a51991
yoga <yoga_email@yahoo.com>
2016-11-03 13:47:39 +0700

    jadikan sms tiap jam 14

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends ConsoleKernel
 				  ->monthlyOn(date('t'), '15:00');
 
 		 $schedule->command('sms:angkakontak')
-					->dailyAt('12:00');
+					->dailyAt('14:00');
 		 //$schedule->command('sms:kontak')
 				  //->everyMinute();
     }
