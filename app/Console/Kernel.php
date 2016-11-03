d7ca5bd3eda8f1627a956c582ceeb655f847e3e5
yoga <yoga_email@yahoo.com>
2016-11-03 15:11:18 +0700

    kjfdk

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends ConsoleKernel
 				  ->monthlyOn(date('t'), '15:00');
 
 		 $schedule->command('sms:angkakontak')
-					->dailyAt('15:10'); 
+					->dailyAt('15:20'); 
 		 //$schedule->command('sms:kontak')
 				  //->everyMinute();
     }
