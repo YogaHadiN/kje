d41a8d7b8b4ac62f8be48ff42105ef0e3f3cce1e
yoga <yoga_email@yahoo.com>
2016-11-12 10:02:35 +0700

    perbaiki test periksa hilang

--- app/Console/Commands/testPeriksahilang.php
+++ app/Console/Commands/testPeriksahilang.php
@@ -54,45 +54,6 @@ class testPeriksahilang extends Command
 			} catch (\Exception $e) {
 				$errors[] = $rc ;
 			}
-            $valid = false;
-            foreach ($arrs as $key => $ar) {
-                if ( $key > 0 && $arrs[$key-1]->coa_id == 110000 && $arrs[$key-1]->debit == 1 ){
-                    $valid = true;
-                }
-                if ($valid && $ar->debit == 0) {
-                    $sama = false;
-                    foreach ($table as $k=> $tab) {
-                        if( $tab['coa_id'] == $ar->coa_id){
-                            $table[$k]['nilai'] = $tab['nilai'] + $ar->nilai;
-                            $table[$k]['jumlah'] = $tab['jumlah'] + 1;
-                            $sama = true;
-                            $id_sama = false;
-                            foreach ($tab['jurnalable_id'] as $jurnl) {
-                                if ($jurnl == $rc->jurnalable_id) {
-                                    $id_sama = true;
-                                }
-                            }
-                            if (!$id_sama) {
-                                $table[$k]['jurnalable_id'][] = $rc->jurnalable_id;
-                            }
-                        }
-                    }
-                    if (!$sama) {
-                        $table[] =[
-                            'id' => $ar->id,
-                            'coa_id' => $ar->coa_id,
-                            'coa'    => $ar->coa->coa,
-                            'nilai'  => $ar->nilai,
-                            'jumlah' => 1,
-                            'jurnalable_id' => [
-                                 $rc->jurnalable_id
-                            ]
-                        ]; 
-                    }
-                } else if ($ar->debit == 1 && $ar->coa_id != 110000 ) {
-                        break;
-                }
-            }
         }
 		return dd( $errors );
     }
