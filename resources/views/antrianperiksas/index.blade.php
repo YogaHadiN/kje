83c1572c522e386c727798e0348e721f4d8c8c06
yoga <yoga_email@yahoo.com>
2016-11-21 22:56:01 +0700

    qr code

--- resources/views/antrianperiksas/index.blade.php
+++ resources/views/antrianperiksas/index.blade.php
@@ -180,6 +180,13 @@ Klinik Jati Elok | Poli {!! ucfirst($poli) !!}
 			  </div>
           </div>
     </div>
+	@if($poli == 'estetika')
+		<div class="row">
+			<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+				<img src="{!! url('img/qr.png') !!}? {{ time() }}" alt="" width="300px" height="300px">
+			</div>
+		</div>
+	@endif
 @endif
 @include('antrianpolis.modalalasan')
 @stop
