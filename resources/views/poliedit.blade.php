9978e9eb50ce21dd0dd103cacd1568b9a1488897
yoga <yoga_email@yahoo.com>
2016-11-24 22:37:36 +0700

    enable programmatic qrcode

--- resources/views/poliedit.blade.php
+++ resources/views/poliedit.blade.php
@@ -109,7 +109,12 @@ Klinik Jati Elok | Poli {!! ucfirst($antrianperiksa->poli)!!} (Edit)
 	<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
 	</div>
 	<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-	<img src="{!! url( 'qrcode?text=' . $url . '/periksa/' . $periksaex->id . '/images' ) !!}" alt="">
+		<div class="panel panel-default">
+			<div class="panel-body text-center">
+				<h2>Scan disini untuk input gambar</h2>
+				<img src="{!! url( 'qrcode?text=' . $url . '/periksa/' . $periksaex->id . '/images' ) !!}" alt="">
+			</div>
+		</div>
 	</div>
 </div>
 @endif
