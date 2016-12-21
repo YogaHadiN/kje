7e7d47209ed47b480a3667762f76f717e2660ce0
yoga <yoga_email@yahoo.com>
2016-12-21 18:05:45 +0700

    idem

--- resources/views/periksas/show.blade.php
+++ resources/views/periksas/show.blade.php
@@ -76,18 +76,20 @@ Klinik Jati Elok | Pasien
                         </tr>
                         <tr>
                             <td>
-								<div class="panelLeft">
-									  <h2>Transaksi : </h2>
-								</div>
-								<div class="panelRight">
-									<a class="" href="{{ url('periksas/' .$periksa->id . '/edit/transaksiPeriksa') }}
-										
-										@if( \Auth::id() != '28' )
-											disabled
-										@endif
-										">
-										ed
-									</a>
+								<div class="row">
+									<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+										  <h2>Transaksi : </h2>
+									</div>
+									<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+										<a class="btn btn-warning " href="{{ url('periksas/' .$periksa->id . '/edit/transaksiPeriksa') }}
+											
+											@if( \Auth::id() != '28' )
+												disabled
+											@endif
+											">
+											Edit
+										</a>
+									</div>
 								</div>
                               <table class="table table-condensed">
                                 <tbody>
