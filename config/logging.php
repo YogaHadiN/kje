a4ba1239447602bb7266ba74bd5ca432bb2fab56
YogaHadiN <yoga_email@yahoo.com>
2022-02-20 00:25:38 +0700

    oke

--- config/logging.php
+++ config/logging.php
@@ -65,6 +65,7 @@ return [
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => 14,
+            'permission' => 0664,
         ],
 
         'slack' => [
