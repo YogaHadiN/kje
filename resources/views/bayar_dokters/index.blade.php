ee8ce692f0021d151dc409821805ef210aba0f2a
YogaHadiN <yoga_email@yahoo.com>
2022-03-12 17:18:19 +0700

    panel

--- resources/views/bayar_dokters/index.blade.php
+++ resources/views/bayar_dokters/index.blade.php
@@ -20,50 +20,37 @@
 
 @stop
 @section('content') 
-
-<div class="panel panel-primary">
-      <div class="panel-heading">
-            <div class="panel-title">
-                <div class="panelLeft">
-                </div>
-                <div class="panelRight">
-                </div>
-            </div>
-      </div>
-      <div class="panel-body">
-		  <div class="table-responsive">
-            <table class="table table-striped table-bordered table-hover " id="tableAsuransi">
-                  <thead>
-                    <tr>
-                    	<th>ID</th>
-                    	<th>
-                            Tanggal
-                            {!! Form::text('id', null, ['class' => 'ajaxChange form-control', 'id' => 'id']) !!}
-                        </th>
-                    	<th>
-                            Nama Dokter
-                            {!! Form::text('nama_dokter', null, ['class' => 'ajaxChange form-control', 'id' => 'nama_dokter']) !!}
-                        </th>
-                    	<th>
-                            Pembayaran
-                            {!! Form::text('pembayaran', null, ['class' => 'ajaxChange form-control', 'id' => 'pembayaran']) !!}
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($bayardokters as $bayar)
-                        <tr>
-                            <td>{!! $bayar->id !!}</td>
-                            <td>{!! $bayar->created_at !!}</td>
-                            <td>{!! $bayar->nama !!}</td>
-                            <td class="uang">{!! $bayar->nilai !!}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-		  </div>
-      </div>
-</div>
+  <div class="table-responsive">
+    <table class="table table-striped table-bordered table-hover " id="tableAsuransi">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>
+            Tanggal
+            {!! Form::text('id', null, ['class' => 'ajaxChange form-control', 'id' => 'id']) !!}
+          </th>
+          <th>
+            Nama Dokter
+            {!! Form::text('nama_dokter', null, ['class' => 'ajaxChange form-control', 'id' => 'nama_dokter']) !!}
+          </th>
+          <th>
+            Pembayaran
+            {!! Form::text('pembayaran', null, ['class' => 'ajaxChange form-control', 'id' => 'pembayaran']) !!}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($bayardokters as $bayar)
+          <tr>
+            <td>{!! $bayar->id !!}</td>
+            <td>{!! $bayar->created_at !!}</td>
+            <td>{!! $bayar->nama !!}</td>
+            <td class="uang">{!! $bayar->nilai !!}</td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
 @stop
 @section('footer') 
 	
