a16f0aa59886bd45c9f696372a706405f0c9b8a9
YogaHadiN <yoga_email@yahoo.com>
2022-03-26 11:05:17 +0700

    koreksi

--- app/Http/Controllers/KasirBaseController.php
+++ app/Http/Controllers/KasirBaseController.php
@@ -102,128 +102,127 @@ class KasirBaseController extends Controller
 
 
 	public function kasir_submit(){
+		$periksa_id    = Input::get('periksa_id');
+		$prx           = Periksa::find($periksa_id);
+		$antrianapotek = AntrianApotek::where('periksa_id', $periksa_id)->first();
+
+		if ( is_null($antrianapotek)) {
+			$pesan = Yoga::gagalFlash('Antrian apotek sudah diproses sebelumnya');
+			return redirect()->back()->withPesan($pesan);
+		}
+
 		DB::beginTransaction();
 		try {
-			$periksa_id    = Input::get('periksa_id');
-			$prx           = Periksa::find($periksa_id);
-			$antrianapotek = AntrianApotek::where('periksa_id', $periksa_id)->first();
-			if ( 
-				!is_null($antrianapotek)
-			) {
 
-				/* if ( */ 
-				/* 	JurnalUmum::where('jurnalable_type', 'App\Periksa') */
-				/* 				->where('jurnalable_id', $periksa_id) */
-				/* 				->count() > 0 */
-				/* ) { */
-				/* 	return redirect('antriankasirs')->withPesan( Yoga::gagalFlash('Pasien sudah pernah diperiksa dan sudah selesai dari Kasir, karena sudah ada catatan di Pembukuan. Bagaimana mungkin bisa dimasukkan lagi? Ceritakan pada dr. Yoga bagaimana pasien ini bisa disubmit lagi di kasir. Karena errornya gak ketemu') ); */
-				/* 	 Sms::send(env("NO_HP_OWNER"),'Kejadian LAGI!!! Kasir input 2 kali!!! Tanyakan!!!' ); */
-				/* } */
-				/* if (AntrianApotek::where('periksa_id', $prx->id)->exists()) { */
-				/* 	$pesan = Yoga::gagalFlash('Pasien sudah melewati proses apotek, tidak perlu diulangi lagi'); */
-				/* 	return redirect('antriankasirs')->withPesan($pesan); */
-				/* } */
+			/* if ( */ 
+			/* 	JurnalUmum::where('jurnalable_type', 'App\Periksa') */
+			/* 				->where('jurnalable_id', $periksa_id) */
+			/* 				->count() > 0 */
+			/* ) { */
+			/* 	return redirect('antriankasirs')->withPesan( Yoga::gagalFlash('Pasien sudah pernah diperiksa dan sudah selesai dari Kasir, karena sudah ada catatan di Pembukuan. Bagaimana mungkin bisa dimasukkan lagi? Ceritakan pada dr. Yoga bagaimana pasien ini bisa disubmit lagi di kasir. Karena errornya gak ketemu') ); */
+			/* 	 Sms::send(env("NO_HP_OWNER"),'Kejadian LAGI!!! Kasir input 2 kali!!! Tanyakan!!!' ); */
+			/* } */
+			/* if (AntrianApotek::where('periksa_id', $prx->id)->exists()) { */
+			/* 	$pesan = Yoga::gagalFlash('Pasien sudah melewati proses apotek, tidak perlu diulangi lagi'); */
+			/* 	return redirect('antriankasirs')->withPesan($pesan); */
+			/* } */
 
 
 
-				//rubah terapi sesuai yang sudah diubah
-				$terapi1        = Input::get('terapi1');
-				$terapi1        = json_decode($terapi1, true);
-				$array          = [];
-				$hargaTotalObat = 0;
-				$arrays         = [];
-				foreach ($terapi1 as $t) {
-					Terapi::where('id', $t['id'])->update([
-						'merek_id'          => $t['merek_id'],
-						'jumlah'            => $t['jumlah'],
-						'harga_beli_satuan' => $t['harga_beli_satuan'],
-						'harga_jual_satuan' => $t['harga_jual_satuan']
-					]);
-					$hargaTotalObat += $t['harga_jual_satuan'] * $t['jumlah'];
-					$arrays[] = [
+			//rubah terapi sesuai yang sudah diubah
+			$terapi1        = Input::get('terapi1');
+			$terapi1        = json_decode($terapi1, true);
+			$array          = [];
+			$hargaTotalObat = 0;
+			$arrays         = [];
+			foreach ($terapi1 as $t) {
+				Terapi::where('id', $t['id'])->update([
+					'merek_id'          => $t['merek_id'],
+					'jumlah'            => $t['jumlah'],
+					'harga_beli_satuan' => $t['harga_beli_satuan'],
+					'harga_jual_satuan' => $t['harga_jual_satuan']
+				]);
+				$hargaTotalObat += $t['harga_jual_satuan'] * $t['jumlah'];
+				$arrays[] = [
 
-						'merek' => Merek::find($t['merek_id'])->merek,
-						'jumlah' => $t['jumlah'],
-						'harga_jual_satuan' => $t['harga_jual_satuan']
-					];
-				}
-
-				//rubah harga obat sesuai dengan terapi yang sudah diubah
-				//
-				//
-
-				if ($prx->poli != 'estetika') {
-					$hargaTotalObat = Yoga::rataAtas5000( $hargaTotalObat );
-				}
-
-				$transaksi = $prx->transaksi;
-				$transaksi = json_decode($transaksi, true);
-				foreach ($transaksi as $k=> $tr) {
-					if ($tr['jenis_tarif'] == 'Biaya Obat') {
-						$transaksi[$k]['biaya'] = $hargaTotalObat;
-					}
-				}
-
-				$prx->transaksi    = json_encode( $transaksi );
-				$prx->save();
-
-				if ( ( Input::get('tacc') ) && Input::get('tacc') == '1' ) {
-					$rjk                    = $prx->rujukan;
-					$rjk->time              = Input::get('time_tacc') ;
-					$rjk->age               = Input::get('age_tacc') ;
-					$rjk->complication      = Input::get('complication_tacc') ;
-					$rjk->comorbidity       = Input::get('comorbidity_tacc') ;
-					$rjk->tacc              = 1;
-					$rjk->save();
-
-					$tidakdirujuk           = Tidakdirujuk::firstOrNew(['icd10_id' => $prx->diagnosa->icd10_id]);
-					$tidakdirujuk->diagnosa = $prx->diagnosa->diagnosa ;
-					$tidakdirujuk->save();
-				}
-
-				//jika ada perbaikan terapi di apotek, masukkam ke dalam database
-				if (!empty(Input::get('terapi2'))) {
-					$perbaikan             = new Perbaikanresep;
-					$perbaikan->periksa_id = $periksa_id;
-					$perbaikan->terapi     = Input::get('terapi1');
-					$perbaikan->save();
-				}
-
-
-				$antriankasir             = new AntrianKasir;
-				$antriankasir->periksa_id = $periksa_id;
-				$antriankasir->jam        = date('H:i:s');
-				$antriankasir->tanggal    = date('Y-m-d');
-				$antriankasir->save();
-
-				Antrian::where('antriable_type', 'App\Models\AntrianApotek')
-						->where('antriable_id', $antrianapotek->id)
-						->update([
-							'antriable_type' => 'App\Models\AntrianKasir',
-							'antriable_id' => $antriankasir->id
-						]);
-
-				PengantarPasien::where('antarable_type', 'App\Models\AntrianApotek')
-						->where('antarable_id', $antrianapotek->id)
-						->update([
-							'antarable_type' => 'App\Models\AntrianKasir',
-							'antarable_id'   => $antriankasir->id
-						]);
-
-				$antrianapotek->delete();
-
-				$apc = new AntrianPolisController;
-				$apc->updateJumlahAntrian(false);
-
-				return redirect('antrianapoteks')
-					->with('pesan', Yoga::suksesFlash('Resep pasien periksa ' . $prx->id. ' <strong>' . $prx->pasien->id . ' - ' . $prx->pasien->nama . '</strong> telah dicetak'))
-					->with('kasir_submit', $periksa_id);
-
-			} else {
-				$pesan = Yoga::gagalFlash('Antrian apotek sudah diproses sebelumnya');
-				return redirect()->back()->withPesan($pesan);
+					'merek' => Merek::find($t['merek_id'])->merek,
+					'jumlah' => $t['jumlah'],
+					'harga_jual_satuan' => $t['harga_jual_satuan']
+				];
 			}
+
+			//rubah harga obat sesuai dengan terapi yang sudah diubah
+			//
+			//
+
+			if ($prx->poli != 'estetika') {
+				$hargaTotalObat = Yoga::rataAtas5000( $hargaTotalObat );
+			}
+
+			$transaksi = $prx->transaksi;
+			$transaksi = json_decode($transaksi, true);
+			foreach ($transaksi as $k=> $tr) {
+				if ($tr['jenis_tarif'] == 'Biaya Obat') {
+					$transaksi[$k]['biaya'] = $hargaTotalObat;
+				}
+			}
+
+			$prx->transaksi    = json_encode( $transaksi );
+			$prx->save();
+
+			if ( ( Input::get('tacc') ) && Input::get('tacc') == '1' ) {
+				$rjk                    = $prx->rujukan;
+				$rjk->time              = Input::get('time_tacc') ;
+				$rjk->age               = Input::get('age_tacc') ;
+				$rjk->complication      = Input::get('complication_tacc') ;
+				$rjk->comorbidity       = Input::get('comorbidity_tacc') ;
+				$rjk->tacc              = 1;
+				$rjk->save();
+
+				$tidakdirujuk           = Tidakdirujuk::firstOrNew(['icd10_id' => $prx->diagnosa->icd10_id]);
+				$tidakdirujuk->diagnosa = $prx->diagnosa->diagnosa ;
+				$tidakdirujuk->save();
+			}
+
+			//jika ada perbaikan terapi di apotek, masukkam ke dalam database
+			if (!empty(Input::get('terapi2'))) {
+				$perbaikan             = new Perbaikanresep;
+				$perbaikan->periksa_id = $periksa_id;
+				$perbaikan->terapi     = Input::get('terapi1');
+				$perbaikan->save();
+			}
+
+
+			$antriankasir             = new AntrianKasir;
+			$antriankasir->periksa_id = $periksa_id;
+			$antriankasir->jam        = date('H:i:s');
+			$antriankasir->tanggal    = date('Y-m-d');
+			$antriankasir->save();
+
+			Antrian::where('antriable_type', 'App\Models\AntrianApotek')
+					->where('antriable_id', $antrianapotek->id)
+					->update([
+						'antriable_type' => 'App\Models\AntrianKasir',
+						'antriable_id' => $antriankasir->id
+					]);
+
+			PengantarPasien::where('antarable_type', 'App\Models\AntrianApotek')
+					->where('antarable_id', $antrianapotek->id)
+					->update([
+						'antarable_type' => 'App\Models\AntrianKasir',
+						'antarable_id'   => $antriankasir->id
+					]);
+
+			$antrianapotek->delete();
+
+			$apc = new AntrianPolisController;
+			$apc->updateJumlahAntrian(false);
+
 			DB::commit();
+			return redirect('antrianapoteks')
+				->with('pesan', Yoga::suksesFlash('Resep pasien periksa ' . $prx->id. ' <strong>' . $prx->pasien->id . ' - ' . $prx->pasien->nama . '</strong> telah dicetak'))
+				->with('kasir_submit', $periksa_id);
+
 		} catch (\Exception $e) {
 			DB::rollback();
 			throw $e;
