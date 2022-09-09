1e542f1d98643e6a7c5b10efb44d681de512bc7d
YogaHadiN <yoga_email@yahoo.com>
2022-09-09 20:54:08 +0700

    upper

--- app/Http/Controllers/AntrianPeriksasController.php
+++ app/Http/Controllers/AntrianPeriksasController.php
@@ -136,6 +136,7 @@ class AntrianPeriksasController extends Controller
 	 * @return Response
 	 */
 	public function store(Request $request, $id) {
+        dd(Input::all()); 
         DB::beginTransaction();
         try {
             $antrianpoli = AntrianPoli::with('pasien', 'asuransi')->where( 'id',  $id )->where('submitted', '0')->first();
@@ -146,9 +147,9 @@ class AntrianPeriksasController extends Controller
             $rules = [
                 'asisten_id'             => 'required',
                 'gds'                    => Rule::requiredIf( $this->cekGDSDiNurseStation($antrianpoli) ),
-                'g'                      => Rule::requiredIf( Input::get('hamil') == 1  ),
-                'p'                      => Rule::requiredIf( Input::get('hamil') == 1  ),
-                'a'                      => Rule::requiredIf( Input::get('hamil') == 1  ),
+                'G'                      => Rule::requiredIf( Input::get('hamil') == 1  ),
+                'P'                      => Rule::requiredIf( Input::get('hamil') == 1  ),
+                'A'                      => Rule::requiredIf( Input::get('hamil') == 1  ),
                 'hpht'                   => Rule::requiredIf( Input::get('hamil') == 1  ),
                 'sistolik'               => Rule::requiredIf( $this->harusCekTekananDarah($antrianpoli) ),
                 'diastolik'              => Rule::requiredIf( $this->harusCekTekananDarah($antrianpoli) ),
@@ -163,8 +164,8 @@ class AntrianPeriksasController extends Controller
             ];
 
             $validator = \Validator::make(Input::all(), $rules, [
-                'gds.required' => 'Gula Darah Harus Diisi Karena pasien ini prolanis DM',
-                'sistolik.required' => 'Sistolik Harus Diisi Karena pasien ini prolanis HT',
+                'gds.required'       => 'Gula Darah Harus Diisi Karena pasien ini prolanis DM',
+                'sistolik.required'  => 'Sistolik Harus Diisi Karena pasien ini prolanis HT',
                 'diastolik.required' => 'Diastolik Harus Diisi Karena pasien ini prolanis HT',
             ]);
 
@@ -234,9 +235,9 @@ class AntrianPeriksasController extends Controller
             $ap->diastolik                   = $this->input_diastolik;
             $ap->tinggi_badan                = $this->input_tinggi_badan;
             $ap->gds                         = $this->input_gds;
-            $ap->g                           = Input::get('g');
-            $ap->p                           = Input::get('p');
-            $ap->a                           = Input::get('a');
+            $ap->g                           = Input::get('G');
+            $ap->p                           = Input::get('P');
+            $ap->a                           = Input::get('A');
             $ap->hpht                        = !empty( Input::get('hpht') ) ? Carbon::createFromFormat('d-m-Y', Input::get('hpht'))->format('Y-m-d') :null;
             $ap->previous_complaint_resolved = Input::get('previous_complaint_resolved');
             $ap->perujuk_id                  = $this->input_perujuk_id;
