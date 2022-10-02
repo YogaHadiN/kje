6a2c0969dec101225c89245281a475357c888030
YogaHadiN <yoga_email@yahoo.com>
2022-10-02 13:11:15 +0700

    db seed complete

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(UpdateFormulaCunamIdTableSeeder::class);
+        /* $this->call(UpdateFormulaCunamIdTableSeeder::class); */
         /* $this->call(VerifikasiTableSeeder::class); */
     }
 }
