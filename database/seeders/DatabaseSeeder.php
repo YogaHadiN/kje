01cafea23f231cdcbe995eb031067254baf08096
YogaHadiN <yoga_email@yahoo.com>
2022-11-14 14:38:16 +0700

    ten2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,11 +5,6 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use DB;
 use App\Models\Tenant;
-use App\Models\Staf;
-use App\Models\Merek;
-use App\Models\TipeFormula;
-use App\Models\JenisTarif;
-use App\Models\TipeJenisTarif;
 
 class DatabaseSeeder extends Seeder
 {
@@ -26,6 +21,9 @@ class DatabaseSeeder extends Seeder
             'address_line1' => 'Komplek Bumi Jati Elok Blok A I No. 7',
             'address_line2' => 'Jl. Raya Legok - Parung Panjang km. 3',
             'address_line3' => 'Malangnengah, Pagedangan, Tangerang, Banten Telp 021 5977529',
-        ])
+        ]);
+        Tenant::where('id', 2)->update([
+            'address' => 'Perumnas Bumi Serpong Suradita, Jl. Wijaya Kusuma Raya 10, Suradita, Cisauk, Tangerang, Banten, Telp 02175790824',
+        ]);
     }
 }
