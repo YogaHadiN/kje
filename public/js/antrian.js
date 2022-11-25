de79b840d98abfe5285cd64a878d8900640e071a
YogaHadiN <yoga_email@yahoo.com>
2022-11-25 17:25:52 +0700

    error

--- public/js/antrian.js
+++ public/js/antrian.js
@@ -125,12 +125,6 @@ function showNotificationWhenError(jqXHR) {
         alert("Requested page not found. [404]");
     } else if (jqXHR.status == 500) {
         alert("Internal Server Error [500].");
-    } else if (exception === "parsererror") {
-        alert("Requested JSON parse failed.");
-    } else if (exception === "timeout") {
-        alert("Time out error.");
-    } else if (exception === "abort") {
-        alert("Ajax request aborted.");
     } else {
         alert("Uncaught Error.\n" + jqXHR.responseText);
     }
