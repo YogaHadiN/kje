d6e6aa5fa74bbe44a52ff0cdcaace43710d371c4
YogaHadiN <yoga_email@yahoo.com>
2022-12-08 19:23:49 +0700

    oke

--- public/js/bayar_dokters.js
+++ public/js/bayar_dokters.js
@@ -19,6 +19,7 @@ function view(key = 0) {
         var key = hasil.key;
         var rows = hasil.rows;
         var temp = "";
+        var token = $('meta[name="csrf-token"]').attr("content");
         for (var i = 0; i < data.length; i++) {
             temp += "<tr>";
             temp += "<td>" + data[i].id + "</td>";
@@ -26,12 +27,30 @@ function view(key = 0) {
             temp += "<td>" + data[i].nama_dokter + "</td>";
             temp +=
                 "<td class='text-right'>" + uang(data[i].pembayaran) + "</td>";
+            temp += "<td>";
             temp +=
-                "<td><a class='btn btn-info btn-xs' href='" +
+                '<form method="POST" action="' +
+                base +
+                "/bayar_gajis/" +
+                data[i].id +
+                '" accept-charset="UTF-8"> <input name="_method" type="hidden" value="DELETE" autocomplete="off"> <input name="_token" type="hidden" value="' +
+                token +
+                '" autocomplete="off">';
+            temp +=
+                " <a class='btn btn-info btn-xs' href='" +
                 base +
                 "/pdfs/bayar_gaji_karyawan/" +
                 data[i].id +
-                "' target='_blank'>Struk</a></td>";
+                "' target='_blank'>Struk</a>";
+            temp +=
+                '<input class="btn btn-danger btn-xs ml-6" onclick="return confirm(\'Anda yakin mau menghapus gaji ' +
+                data[i].nama_dokter +
+                " pada tanggal " +
+                data[i].tanggal +
+                " dengan id " +
+                data[i].id +
+                '?\');return false;" type="submit" value="Delete" autocomplete="off">';
+            temp += "</form>";
             temp += "</tr>";
         }
         $("#content").html(temp);
--- resources/views/bayar_dokters/index.blade.php
+++ resources/views/bayar_dokters/index.blade.php
@@ -17,65 +17,11 @@
           <strong>Pembayaran Dokter</strong>
       </li>
 </ol>
-
 @stop
 @section('content') 
-  <div class="table-responsive">
-    <div class="row">
-        <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-            Ditemukan <span id="rows_found"></span> data
-        </div>
-        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-            {!! Form::select('name', App\Models\Classes\Yoga::manyRows(), 15, [
-                'class'    => 'form-control',
-                'id'       => 'displayed_rows',
-                'onchange' => 'clearAndView();return false;'
-            ]) !!}
-        </div>
-    </div>
-    <table class="table table-striped table-bordered table-hover " id="tableAsuransi">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>
-            Tanggal
-            {!! Form::text('tanggal', null, [
-              'class' => 'ajaxChange form-control', 
-              'onkeyup' => 'clearAndView();return false;',
-              'id' => 'tanggal'
-            ]) !!}
-          </th>
-          <th>
-            Nama Dokter
-            {!! Form::text('nama_dokter', null, [
-              'class'   => 'ajaxChange form-control',
-              'onkeyup' => 'clearAndView();return false;',
-              'id'      => 'nama_dokter'
-            ]) !!}
-          </th>
-          <th>
-            Pembayaran
-            {!! Form::text('pembayaran', null, [
-              'class'   => 'ajaxChange form-control',
-              'style'=>"text-align:right;",
-              'onkeyup' => 'clearAndView();return false;',
-              'id'      => 'pembayaran']) !!}
-          </th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody id="content"></tbody>
-    </table>
-  </div>
-  <div id="page-box">
-      <nav class="text-right" aria-label="Page navigation" id="paging">
-      
-      </nav>
-  </div>
+ @include('bayar_dokters.form')
 @stop
 @section('footer') 
 	<script src="{!! url('js/twbs-pagination/jquery.twbsPagination.min.js') !!}"></script>
 	{!! HTML::script('js/bayar_dokters.js')!!}
 @stop
-
-
