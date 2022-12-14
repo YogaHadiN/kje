044bb4b76f3b59182a81078c9f64264c290f6206
YogaHadiN <yoga_email@yahoo.com>
2022-12-14 17:44:46 +0700

    oke

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,70 +16,10 @@ use App\Models\RecoveryIndex;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        FrekuensiCek::create([
-            'frekuensi_cek' => 'harian'
-        ]);
-        FrekuensiCek::create([
-            'frekuensi_cek' => 'mingguan'
-        ]);
-        FrekuensiCek::create([
-            'frekuensi_cek' => 'bulanan'
-        ]);
-        FrekuensiCek::create([
-            'frekuensi_cek' => 'tahunan'
-        ]);
-
-        CekList::create([
-            'cek_list'             => 'jumlah epinefrin inj',
-        ]);
-        CekList::create([
-            'cek_list'             => 'jumlah dexamethasone inj',
-        ]);
-        CekList::create([
-            'cek_list'             => 'jumlah ranitidine inj',
-        ]);
-        CekList::create([
-            'cek_list'             => 'jumlah diphenhydramine inj',
-        ]);
-        CekList::create([
-            'cek_list'             => 'jumlah spuit 3cc',
-        ]);
-        CekList::create([
-            'cek_list'             => 'suhu',
-        ]);
-        CekList::create([
-            'cek_list'             => 'oksigen bisa digunakan',
-        ]);
-        CekList::create([
-            'cek_list'             => 'jumlah gudel',
-        ]);
-        CekList::create([
-            'cek_list'             => 'jumlah_infus_set',
-        ]);
-        CekList::create([
-            'cek_list'             => 'jumlah_nacl',
-        ]);
-        CekList::create([
-            'cek_list'             => 'jumlah_tiang_Infus',
-        ]);
-
-        Limit::create([
-            'limit' => 'minimal'
-        ]);
-
-        Limit::create([
-            'limit' => 'maksimal'
-        ]);
-
-        Limit::create([
-            'limit' => 'sama dengan'
-        ]);
+        $tenant = Tenant::find(2);
+        $tenant->exp_date_validation_available = 0;
+        $tenant->save();
     }
 }
