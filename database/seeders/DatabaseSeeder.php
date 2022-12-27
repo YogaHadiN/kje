e975deb5577920b31fb5288a7615b1a1bf0b3d1d
YogaHadiN <yoga_email@yahoo.com>
2022-12-27 14:01:29 +0700

    seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,19 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        $tenant = Tenant::find(2);
-        $tenant->exp_date_validation_available = 0;
-        $tenant->save();
+        DB::update("update whatsapp_complaints set tenant_id = 1;");
+        DB::update("update recovery_index set tenant_id = 1;");
+        DB::update("update failed_therapies set tenant_id = 1;");
+        DB::update("update whatsapp_cek_list_harians set tenant_id = 1;");
+        DB::update("update cek_list_harians set tenant_id = 1;");
+        DB::update("update cek_list_ruangans set tenant_id = 1;");
+        DB::update("update perusahaans set tenant_id = 1;");
+        DB::update("update peserta_bpjs_perusahaans set tenant_id = 1;");
+        DB::update("update whatsapp_satisfaction_surveys set tenant_id = 1;");
+        DB::update("update whatsapp_recovery_indices set tenant_id = 1;");
+        DB::update("update kuesioner_menunggu_obats set tenant_id = 1;");
+        DB::update("update whatsapp_main_menus set tenant_id = 1;");
+        DB::update("update whatsapp_bpjs_dentist_registrations set tenant_id = 1;");
+        DB::update("update dentist_reservations set tenant_id = 1;");
     }
 }
