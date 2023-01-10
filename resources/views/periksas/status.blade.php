e3cce46a8978acd97a6660591fc568e5b7368687
YogaHadiN <yoga_email@yahoo.com>
2023-01-10 16:21:45 +0700

    dfasdf

--- resources/views/periksas/status.blade.php
+++ resources/views/periksas/status.blade.php
@@ -41,9 +41,9 @@
 							{!! $periksa->sistolik !!}/{!! $periksa->diastolik !!} mmHg  <br>
 						@endif
                         <strong>Suhu</strong><br>
-                        {!! $periksa->suhu ? $perik->suhu . '<sup>o</sup>C' : 'Tidak diisi' !!}<br>
+                        {!! $periksa->suhu ? $periksa->suhu . '<sup>o</sup>C' : 'Tidak diisi' !!}<br>
                         <strong>Berat Badan</strong><br>
-                        {!! $periksa->berat_badan ? $perik->berat_badan . ' kg' : 'Tidak diisi' !!}<br>
+                        {!! $periksa->berat_badan ? $periksa->berat_badan . ' kg' : 'Tidak diisi' !!}<br>
 						<strong>Diagnosa :</strong> <br>
 						{!! $periksa->diagnosa->diagnosa !!} - {!! $periksa->diagnosa->icd10_id !!} ({!! $periksa->diagnosa->icd10->diagnosaICD !!})
                         @if( !empty( trim( $periksa->keterangan_diagnosa ) ) )
