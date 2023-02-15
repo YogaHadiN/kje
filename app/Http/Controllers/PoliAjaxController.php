27d6227c76b53a084d7f2262b293ef7719349f13
YogaHadiN <yoga_email@yahoo.com>
2023-02-15 09:50:32 +0700

    clean

--- app/Http/Controllers/PoliAjaxController.php
+++ app/Http/Controllers/PoliAjaxController.php
@@ -746,7 +746,7 @@ class PoliAjaxController extends Controller
      *
      * @return void
      */
-    private function function clean($string) {
+    private function clean($string) {
        $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
        return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
     }    
