eaca0ddc6f65c96daa85cd3218571addd8967020
YogaHadiN <yoga_email@yahoo.com>
2023-02-19 18:50:17 +0700

    ddddd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,15 +6,18 @@ use Illuminate\Database\Seeder;
 use DB;
 use App\Models\Asuransi;
 use App\Models\Staf;
+use App\Models\CekListDikerjakan;
 use App\Http\Controllers\BayarGajiController;
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        Asuransi::whereIn('id', [187,210] )->update([
-            'konfirmasi_obat_paten' => 0
-        ]);
+        foreach (CekListDikerjakan::all() as $cek) {
+            if (!$cek->cekListRuangan) {
+                $cek->delete();
+            }
+        }
     }
 }
 
